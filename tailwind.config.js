a94fdc16fb2e1bb731d67bfac10e472ed006208d
rinnierambler <corinne.laing@hotmail.co.uk>
2024-11-15 14:53:36 +0800

    Custom CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,62 @@ module.exports = {
     './*.{html,js,md}'
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: ['0.75rem', '1.2'],
+      sm: ['0.875rem;', '1.2'],
+      base: ['1rem', '1.2'],
+      lg: ['1.125rem;', '1.2'],
+      xl: ['1.25rem', '1.2'],
+      '2xl': ['1.5rem', '1.2'],
+      '3xl': ['1.875rem', '1'],
+      '4xl': ['2.25rem', '1'],
+      '5xl': ['3rem', '1'],
+      '6xl': ['3.75rem', '1'],
+      '7xl': ['4.5rem', '1'],
+      '8xl': ['6rem', '1'],
+      '9xl': ['8rem', '1'],
+    },
+    fontFamily: {
+      'sans': ['barlow', 'ui-sans-serif'],
+      'sans-semibold': ['barlow semi condensed', 'ui-sans-serif'],
+      'sans-bold': ['barlow condensed','ui-sans-serif'],
+    },
+    colors: {
+      bluedark: '',
+      bluelight: '',
+      light: '',
+    },
+    backgroundPosition: {
+      'bottom-4': 'center bottom 1rem',
+      'top-4': 'center top 1rem',
+    },
+    extend: {
+      lineHeight: {
+        'tight': '.85',
+        'snug': '.95',
+        '12': '3rem',
+      },
+      spacing: {
+        '2p': '2%',
+        '5p': '5%',
+        '10p': '10%',
+        '15p': '15%',
+      },
+      minHeight:{
+        '100': '100vh',
+        '90': '90vh',
+        '80': '80vh',
+        '70': '70vh',
+        '60': '60vh',
+        '50': '50vh',
+      },
+      aspectRatio: {
+        'landscape': '3 / 2',
+        'portrait-tall': '5/8',
+        'portrait': '4/5',
+        '5/3': '5 / 3',
+      },
+    },
   },
   plugins: [],
 }
